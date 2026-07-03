5ff8a03a9708661a1e60f81d8c1b07dba8c01426
ronaldershitao-web <ronald.er.shi.tao@gmail.com>
2026-07-03 12:04:16 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,10 +10,10 @@ const CONFIG = {
     VERSION: "1.0.0",
 
     // Google OAuth Client ID
-    CLIENT_ID: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+    CLIENT_ID: "263902111764-jdil2ttim5q05gim8tsheopgoprr6jmh.apps.googleusercontent.com",
 
     // Apps Script URL
-    API_URL: "YOUR_APPS_SCRIPT_WEBAPP_URL",
+    API_URL: "https://script.google.com/macros/s/AKfycbzxu8s9fLTqhhBJ4S-CoYFXbQNJ5R3SwHBUlSv2JQ0idR0e3aOyhgrGoFNTXqefOiqp/exec",
 
     DEBUG: true
 
