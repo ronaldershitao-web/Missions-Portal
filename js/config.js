a6b7f1855699acbb94d2e95b0fa01e97061376f7
ronaldershitao-web <ronald.er.shi.tao@gmail.com>
2026-07-04 15:34:43 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,19 +1,12 @@
-/* ==========================================
-   Missions Portal
-   Configuration
-========================================== */
-
 const CONFIG = {
 
     APP_NAME: "Missions Portal",
 
     VERSION: "1.0.0",
 
-    // Google OAuth Client ID
     CLIENT_ID: "263902111764-jdil2ttim5q05gim8tsheopgoprr6jmh.apps.googleusercontent.com",
 
-    // Apps Script URL
-    API_URL: "https://script.google.com/macros/s/AKfycbzxu8s9fLTqhhBJ4S-CoYFXbQNJ5R3SwHBUlSv2JQ0idR0e3aOyhgrGoFNTXqefOiqp/exec",
+    DEPLOYMENT_ID: "AKfycbwloXXuUM8zEg4woOdaet0WoFOkTlbEr5gRQv2-yBZ6gcEZ44n2Y1EOON0CBnUoec60",
 
     DEBUG: true
 
