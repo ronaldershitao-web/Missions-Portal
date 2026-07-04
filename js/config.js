5208895f0224fe274f11d0060d723d39dc5e8ed2
ronaldershitao-web <ronald.er.shi.tao@gmail.com>
2026-07-05 05:24:09 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     APP_NAME: "Missions Portal",
     VERSION: "1.0.0",
 
-    CLIENT_ID: "263902111764-jdil2ttim5q05gim8tsheopgoprr6jmh.apps.googleusercontent.com",
+    CLIENT_ID: "263902111764-o17c11p5mj0c9v41dd2rd8886clna5hp.apps.googleusercontent.com",
 
     // THIS MUST BE SCRIPT PROJECT ID (NOT AKfycb)
     SCRIPT_ID: "1P5yyJNmQ6L8zndgkcDmj6h7xoExK_7EDV_jnRIQZjovJK-hGgojlwhd9",
