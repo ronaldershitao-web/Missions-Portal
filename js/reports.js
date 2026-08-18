199c411ac60af1f32bc50605f41b42ec4d959b74
ronaldershitao-web <ronald.er.shi.tao@gmail.com>
2026-08-18 15:58:11 +0800

    Update reports.js

--- js/reports.js
+++ js/reports.js
@@ -1595,10 +1595,10 @@ function renderParticipantJourneySummary() {
 
 
     const people =
-        Dashboard.data
-            ?.tables
-            ?.participantDirectory ||
-        [];
+    Dashboard.data
+        ?.tables
+        ?.participantDirectory ||
+    [];
 
 
     const sorted =
